6082803d9117ce7554c0a1af6ce89d97aae58ee0
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-11-17 12:50:52 -0500

    format apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // coinbase api url
-// https://api.coinbase.com/v2/prices/:currency_pair
+// https://api.coinbase.com/v2/prices/:currency_pair/spot
 
 // quotable io url 
 // https://api.quotable.io/random?tags=
@@ -12,10 +12,22 @@ function submitHandler(currency, amount, date) {
     var currency = document.querySelector("#currency").value;
     var amount = document.querySelector("#amount").value;
     var date = document.querySelector("#date").value;
+    var formatDate = date.split("/");
+    var buyDay = formatDate[2];
+    var buyMonth = formatDate[1];
+    var buyYear = formatDate[3];
 
     console.log(currency, amount, date);
 
+    var currentCoin = "https://api.coinbase.com/v2/prices/:" + currency + "_USD/spot";
+    var userCoin = "https://api.coinbase.com/v2/prices/:" + currency + "_USD/spot?date=" + buyYear + "-" + buyMonth + "-" + buyDay;
+
+    
+
+
 }
+
+
 
 $("#date").datepicker();
 userForm.addEventListener("submit", submitHandler);
