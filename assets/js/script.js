fa603180a67667cbc30737bc65206f2f4ad227c8
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-11-17 21:02:19 -0500

    attempted getting the compare function working

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,29 +12,116 @@
                 // populate quote with generated html
 
 var userForm = document.querySelector("#user-form");
+var currentCoin;
+var userCoin;
 
-function submitHandler(currency, amount, date) {
+var currentCoinResult;
+var userCoinResult;
+
+// var test = {data: {amount: 1}};
+
+// takes info from user input and passes through to fetch request
+function submitHandler(event,currency, amount, date) {
     event.preventDefault();
+
+
 
     var currency = document.querySelector("#currency").value;
     var amount = document.querySelector("#amount").value;
     var date = document.querySelector("#date").value;
     var formatDate = date.split("/");
-    var buyDay = formatDate[2];
-    var buyMonth = formatDate[1];
-    var buyYear = formatDate[3];
+
+    var userInput ={
+        currencyInput:currency,
+        buyDay: formatDate[1],
+        buyMonth: formatDate[0],
+        buyYear: formatDate[2],
+    }
+
 
     console.log(currency, amount, date);
+console.log(userInput);
 
-    var currentCoin = "https://api.coinbase.com/v2/prices/:" + currency + "_USD/spot";
-    var userCoin = "https://api.coinbase.com/v2/prices/:" + currency + "_USD/spot?date=" + buyYear + "-" + buyMonth + "-" + buyDay;
 
+console.log(currentApiCall(userInput))
+ console.log(userApiCall(userInput))
+
+  
+        
+};
+
+function currentApiCall(input){
+    var url = "https://api.coinbase.com/v2/prices/" + input.currencyInput + "-USD/spot";
+    fetch(url)
+        .then(function(response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then( result => console.log(result)
+            );
+            } else {
+                // populate error area? 
+            }
+        })
+        .catch(function(error) {
+            
+            // populate error with unable to connect to coinbase
+        });    
+}
+
+function userApiCall(input){
+var url = "https://api.coinbase.com/v2/prices/" + input.currencyInput + "-USD/spot?date=" + input.buyYear + "-" + input.buyMonth + "-" + input.buyDay;
+fetch(url)
+    .then(function(response) {
+        // request was successful
+        if (response.ok) {
+            response.json().then(function(result) {
+              return result;
+
+        });
+        
+        } else {
+            // populate error area? 
+        }
+    })
+    .catch(function(error) {
+    //    populate error div with unable to connect to coinbase
+    });    
+}
+
+function getCurrentPrice(input) {
+    console.log(input);
+    var price = input.data.amount;
+
+    console.log(price);
+    return price;
+}
+
+function getBuyPrice(input) {
+    console.log(input);
+    var price = input.data.amount;
     
+    console.log(price);
+    return price;
+}
 
+
+function gainOrLoss(a,b) {
+    console.log(a)
+    console.log(b)
+    // var buyPrice = getBuyPrice(userCoin);
+    // console.log(buyPrice);
+
+    // var currentPrice = getCurrentPrice(currentCoin);
+    // console.log(currentPrice);
 
 }
 
 
 
+
 $("#date").datepicker();
 userForm.addEventListener("submit", submitHandler);
+
+
+
+
