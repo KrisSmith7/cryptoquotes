fc7ace264a59a312cfa837d7cf20fe962ff80cab
DSiedlarz90 <dsiedlarz90@gmail.com>
2021-11-19 12:08:10 -0500

    fixed quotes posting to page properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,40 @@
 var qContainer = document.querySelector("#quote-container")
 var happiness = "happiness"
 var wisdom = "wisdom"
-var x = Math.floor(Math.random() * 100)
-console.log(x)
 
-{
-if (x < 50 ){
-    var chooseText = "happiness"
-} else {
-    var chooseText = "wisdom" }
-}
-
-    var quoteURL = "https://api.quotable.io/random?tags=" + chooseText
+function displayQuote(data){
+    var x = Math.floor(Math.random() * 100)
+    console.log(x)
+    if (x < 50 ){
+        var chooseText = "happiness"
+        var quoteURL = "https://api.quotable.io/random?tags=" + chooseText
+        fetch (quoteURL)
+        .then (function (response){
+            return response.json()
+        })
+        .then (function(data){
+            var quoteText = data.content;
+            var quoteEl = document.createElement("p")
+            quoteEl.innerText = "Make that money! -- " + quoteText
+            qContainer.appendChild(quoteEl)
     
-    
-    fetch (quoteURL)
-    .then (function (response){
-        console.log(response);
-        return response.json()
-    })
-    .then (function(data){
-        console.log(data);
-    });
-
-
-function displayQuote (data){
-var quoteText = data.content;
-var quoteEl = document.createElement("p")
-quoteEl.innerText = "Make that money! -- " + quoteText
-qContainer.appendChild(quoteEl)
-}
-
+        })
+    } else {
+        var chooseText = "wisdom" 
+        var quoteURL = "https://api.quotable.io/random?tags=" + chooseText
+        
+        fetch (quoteURL)
+        .then (function (response){
+            return response.json()
+        })
+        .then (function(data){
+            console.log(data.content);
+            var quoteText = data.content;
+            var quoteEl = document.createElement("p")
+            quoteEl.innerText = "Make that money! -- " + quoteText
+            qContainer.appendChild(quoteEl)
+        })
+    };
+};  
 
 displayQuote();
